1e382af38f62f76d626393a227ee5971a67417ce
Ysee MONNIER <yseemonnier@gmail.com>
2016-04-30 23:55:43 +0200

    BillDetail delete object available

--- ProBill/ProBill/Controller/Bills/BillDetailViewController.swift
+++ ProBill/ProBill/Controller/Bills/BillDetailViewController.swift
@@ -8,20 +8,52 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class BillDetailViewController: UIViewController {
     @IBOutlet weak var picture: UIImageView!
     @IBOutlet weak var price: UILabel!
     
     var bill: Bill?
+    var managedObjectContext: NSManagedObjectContext? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+        
+        //Title
+        let label = UILabel(frame: CGRectMake(0, 0, 440, 44))
+        label.backgroundColor = UIColor.clearColor()
+        label.numberOfLines = 0
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "Detail\n\(self.bill!.subCategory.name)"
+        self.navigationItem.titleView = label
+        
+        //Actions button
+        let trashButton = UIBarButtonItem(barButtonSystemItem: .Trash, target: self, action: #selector(BillDetailViewController.deleteObject(_:)))
+        let shareButton = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: #selector(BillDetailViewController.deleteObject(_:)))
+        
+        self.navigationItem.rightBarButtonItems = [trashButton, shareButton]
+        
+        self.picture.image = UIImage(data: self.bill!.picture)
+        self.price.text = String(self.bill!.price) + " Zl"
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func deleteObject(sender: AnyObject) {
+        self.managedObjectContext!.deleteObject(self.bill!)
+        do {
+            try self.managedObjectContext!.save()
+        } catch let error as NSError {
+            print("Error \(object_getClass(self)) \(#function) : \(error.debugDescription))")
+        }
+        self.navigationController?.popViewControllerAnimated(true)
+    }
     
+    func shareObject(sender: AnyObject) {
+        //TODO...
+    }
 }
